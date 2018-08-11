489b45ed5dce96b73510502e36eb04bf7cb5962f
Adrian Moraret <adrian.moraret@yahoo.com>
2018-08-11 15:37:07 -0700

    Added mocked data for top stories

--- website/src/website/components/pages/index/PopularSection/PopularSection.vue
+++ website/src/website/components/pages/index/PopularSection/PopularSection.vue
@@ -3,10 +3,12 @@
     <div class="col-lg-6">
       <small-post v-for="article in articles" :key="article.url" :article="article"></small-post>
     </div> 
-    <div class="col-lg-6">
-      <large-post :article="mainArticle"></large-post>
-    </div>          
-
+    <div class="col-lg-3">
+      <large-post :article="mainArticle1"></large-post>
+    </div>
+    <div class="col-lg-3">
+      <large-post :article="mainArticle2"></large-post>
+    </div>
   </div>
 </template>
 <script>
@@ -35,7 +37,8 @@
       const articleResponse = await fetch("resources/news/NewYorkTimes/top_stories/technology/snapshot.json")
       const articles = await articleResponse.json()
       this.articles = articles.slice(0,3)
-      this.mainArticle = articles[18]
+      this.mainArticle1 = articles[17]
+      this.mainArticle2 = articles[18]
     },
     data : function(){
       return {
