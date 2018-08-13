4a8a5f1815529c3ba22ad1b09a3371c757b900dd
Adrian Moraret <adrian.moraret@yahoo.com>
2018-08-12 18:58:57 -0700

    Some refactoring and added top_stories jobs

--- appengine/app.js
+++ appengine/app.js
@@ -1,46 +1,35 @@
 console.log('In app.js writing message')
 
 const express = require('express')
-const PubSub = require('@google-cloud/pubsub')
-const pubsub = PubSub()
-const topic = pubsub.topic(process.env.PUBSUB_TOPIC)
-const publisher = topic.publisher()
+const { publish } = require('./pubsub/NewsPubSub.js')
+const { isValid, extractMessage } = require('./helper/RequestHelper')
 
 const app = express();
 
 app.get('/news/:source/:category/:section', (req, res) => {
-  const { source, category, section } = req.params
-  if (!valid(req, source, category, section)) {
-    console.log(`Invalid source: ${source} or category: ${category} or section: ${section}`)
+
+  if (!isValid(req)) {
+    console.log(`Invalid request. Path: ${req.path}`)
     res.status(400).send(`Invalid request`)
     return
   }
-  const message = { source: source, category: category, section: section }
-  publish(res, JSON.stringify(message))
-  console.log(`Message published for source: ${source} category: ${category} section: ${section}`)
+
+  const message = extractMessage(req)
+  publish(message,
+    (err) => {
+      console.log(`Gor error while publishing ${message}: ${err}`)
+      res.status(400).send('Gor error while publishing. Check the logs.')
+    },
+    () => {
+      console.log(`Successfully published ${message}`)
+      res.status(200).send(`Successfully published.`)
+    }
+  )
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`App statred and listening on port ${PORT}`);
-});
+  console.log(`App statred and listening on port ${PORT}`)
+})
 
-module.exports = app;
-
-function publish(res, message) {
-  publisher.publish(Buffer.from(message), (err) => {
-    if (err) {
-      console.log(`Got error publishing message: ${JSON.stringify(err)}`)
-      res.status(400).send(`Got error: ${JSON.stringify(err)}`);
-      return;
-    }
-    res.status(200).send(`Success: Message ${JSON.stringify(message)} sent.`);
-  });
-}
-
-function valid(req, source, category, section) {
-  if (req.get('X-AppEngine-Cron') === 'true') {
-    return true
-  }
-  return false
-}
+module.exports = app
