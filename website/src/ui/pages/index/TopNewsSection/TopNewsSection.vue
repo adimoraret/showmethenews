fb55c3c35f8f0c16dd26ee186c9906dbede10ca7
Adrian Moraret <adrian.moraret@yahoo.com>
2018-08-18 23:59:52 -0700

    Added prev and next buttons

--- website/src/ui/pages/index/TopNewsSection/TopNewsSection.vue
+++ website/src/ui/pages/index/TopNewsSection/TopNewsSection.vue
@@ -3,10 +3,33 @@
       <div class="row card-row">
         <SmallVericalPost v-for="article in articles" :key="article.url" :article="article"></SmallVericalPost>
       </div>
+      
       <nav class="pagination">
-        <a v-for="page in pages" :key="page" @click="clickPage(page)"  class="pagination__page" 
-          :class="page == CurrentPage ? 'pagination__page--current' : '' ">{{page}}</a>
+        <a v-if="CurrentPage != 1" href="#latest-news" @click="prevPage()"
+        class="pagination__page pagination__icon pagination__page--next">
+          <i class="ui-arrow-left"></i>
+        </a>
+        <span v-else class="pagination__page pagination__icon pagination__page--next">
+          <i class="ui-arrow-left"></i>
+        </span>
+
+        <span v-for="page in pages" :key="page">
+          <span v-if="CurrentPage === page" class="pagination__page pagination__page--current">{{CurrentPage}}</span>
+          <a v-else href="#latest-news" 
+            @click="clickPage(page)"  
+            class="pagination__page">{{page}}</a>
+        </span>
+
+        <a v-if="CurrentPage != LastPage" href="#latest-news" @click="nextPage()"
+        class="pagination__page pagination__icon pagination__page--next">
+          <i class="ui-arrow-right"></i>
+        </a>
+        <span v-else class="pagination__page pagination__icon pagination__page--next">
+          <i class="ui-arrow-right"></i>
+        </span>
+
       </nav>
+
     </div>
 </template>
 <script>
@@ -16,37 +39,51 @@
 
   const NumberOfArticles = 6
   const newsManager = new NewsManager()
-  const pagination = new Paginaton()
-
 
   export default {
     components: {
       SmallVericalPost: SmallVericalPost
     },
+    watch: {
+      CurrentPage: function(newPage, oldPage){
+        console.log('changed')
+        this.pagination.clickPage(newPage)
+        this.articles = this.pagination.getItems()
+        this.pages = this.pagination.getVisiblePages()
+      }
+    },
     methods: {
       clickPage(pageNumber) {
-        pagination.clickPage(pageNumber)
-        this.pages = pagination.getVisiblePages()
-        this.articles = pagination.getItems()
-        this.CurrentPage = pagination.getCurrentPage()
+        this.CurrentPage = pageNumber
+      },
+      prevPage(){
+        this.pagination.prevPage()
+        this.CurrentPage = this.pagination.getCurrentPage()
+      },
+      nextPage(){
+        this.pagination.nextPage()
+        this.CurrentPage = this.pagination.getCurrentPage()
       }
     },
     created: async function() {
       const {articles} = await newsManager.getTopNewsArticles(this.section)
-      pagination.init({ 
+      this.pagination.init({ 
         visiblePages:3, 
         itemsPerPage:NumberOfArticles, 
         currentPage:this.CurrentPage, 
         items:articles })
       
-      this.articles = pagination.getItems()
-      this.pages = pagination.getVisiblePages()
+      this.articles = this.pagination.getItems()
+      this.pages = this.pagination.getVisiblePages()
+      this.LastPage = this.pagination.getLastPage()
     },
     data : function(){
       return {
         articles: newsManager.getEmptyArticles(NumberOfArticles),
         pages: [],
-        CurrentPage: 1
+        CurrentPage: 1,
+        LastPage: 1,
+        pagination: new Paginaton()
       }
     },
     props: {
