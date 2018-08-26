3a2e71be0776d0927f4d9599564aff23537a41d9
Adrian Moraret <adrian.moraret@yahoo.com>
2018-08-25 17:39:17 -0700

    WIP

--- website/src/ui/components/pagination/Pagination.vue
+++ website/src/ui/components/pagination/Pagination.vue
@@ -8,34 +8,10 @@
 export default {
   name: 'Pagination',
   computed: {
-    getVisiblePages(){
-      if (this.currentPage === 1){
-        return this.createConsecutiveArray(this.visiblePages, 1)
-      }
-      if (this.currentPage === this.totalPages) {
-        return this.createConsecutiveArray(this.visiblePages, this.totalPages - this.visiblePages + 1)
-      }
-      
-
-    }
+   
   },
   methods: {
-    createConsecutiveArray(numberOfElements, start){
-      return Array.from(new Array(this._visiblePages), (x, i) => (i + start))
-    },
-    clickPage(pageNumber) {
-      this.currentPage = pageNumber
-    },
-    prevPage(){
-      if (this.currentPage > 1) {
-        this.currentPage--
-      }
-    },
-    nextPage(){
-      if (this.currentPage < totalPages) {
-        this.currentPage++
-      }
-    }
+    
   },
   props : ['currentPage', 'visiblePages', 'totalPages']
 }
